25d76fd9f23d72307f5fa64c142d602f3b41826e
0xJordan <ahmedjamlan12@gmail.com>
2023-08-10 10:38:39 +0300

    Added functinoality for job description to be expanded

--- src/components/Jobs/JobItem.jsx
+++ src/components/Jobs/JobItem.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import "./JobItem.css";
 import moment from "moment";
 
 const JobItem = ({ jobDetails }) => {
+  const [showRestOfDescription, setShowRestOfDescription] = useState(false);
   return (
     <article className="job-item">
       <div className="company-info">
@@ -15,11 +17,33 @@ const JobItem = ({ jobDetails }) => {
         </div>
         <p className="job-date">🕑 {moment(jobDetails.created_at).fromNow()}</p>{" "}
       </div>
+      <div
+        className="job-details"
+        onClick={() => setShowRestOfDescription(!showRestOfDescription)}
+      >
+        <h3 className="job-description-header">
+          {" "}
+          {!showRestOfDescription ? (
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+              <path d="M10 20A10 10 0 1 0 0 10a10 10 0 0 0 10 10zM8.711 4.3l5.7 5.766L8.7 15.711l-1.4-1.422 4.289-4.242-4.3-4.347z" />
+            </svg>
+          ) : (
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+              <path d="M12 2a10 10 0 1 0 10 10A10.011 10.011 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8.009 8.009 0 0 1-8 8z" />
+              <path d="M12 12.586 8.707 9.293l-1.414 1.414L12 15.414l4.707-4.707-1.414-1.414L12 12.586z" />
+            </svg>
+          )}{" "}
+          Job Description
+        </h3>
+        <p
+          className={`job-description-text ${
+            !showRestOfDescription && "show-compelete-text"
+          }`}
+        >
+          {jobDetails.description}
+        </p>
+      </div>
 
-      <details className="job-details">
-        <summary className="job-description-header">Job Description</summary>
-        <p className="job-description-text">{jobDetails.description}</p>
-      </details>
       <hr />
       <div className="job-info">
         <div className="job-title">{jobDetails.positionTitle}</div>
